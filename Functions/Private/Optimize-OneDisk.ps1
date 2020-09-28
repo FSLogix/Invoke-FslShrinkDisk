a3a9b6446e9b5e9b3c951eba741acf34ae550ded
Jim Moyle <jim.moyle@microsoft.com>
2020-09-28 21:08:24 +0100

    Updated initial dismount to take disk not path

--- Functions/Private/Optimize-OneDisk.ps1
+++ Functions/Private/Optimize-OneDisk.ps1
@@ -48,7 +48,7 @@ function Optimize-OneDisk {
     } # Begin
     PROCESS {
         #In case there are disks left mounted let's try to clean up.
-        Dismount-DiskImage -ImagePath $Path -ErrorAction SilentlyContinue
+        Dismount-DiskImage -ImagePath $Disk -ErrorAction SilentlyContinue
 
         #Get start time for logfile
         $startTime = Get-Date
