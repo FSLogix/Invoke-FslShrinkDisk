a1f13b5cf14b4e0d9380bc687009529937c84850
JimMoyle <github@jimmoyle.com>
2020-10-09 14:47:30 +0100

    BugFix for dismount

--- Invoke-FslShrinkDisk.ps1
+++ Invoke-FslShrinkDisk.ps1
@@ -1797,7 +1797,7 @@ function Optimize-OneDisk {
     } # Begin
     PROCESS {
         #In case there are disks left mounted let's try to clean up.
-        Dismount-DiskImage -ImagePath $Path -ErrorAction SilentlyContinue
+        Dismount-DiskImage -ImagePath $Disk.FullName -ErrorAction SilentlyContinue
 
         #Get start time for logfile
         $startTime = Get-Date
